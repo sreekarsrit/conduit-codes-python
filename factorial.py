55c87f3bf7c03d3b0e89a4eac2ba12c86ac70dfb
sai sreekar sarvade <sreekarsarvade@gmail.com>
2025-03-05 10:03:58 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,14 +1,3 @@
-import tkinter as tk
-def fun():
-    label.config(text="good bye")
-root=tk.Tk()
-root.title("saii")
-label=tk.Label(root,text="Hello")
-label.pack()
-button=tk.Button(root,text="change message",command=fun)
-button.pack()
-root.mainloop()
-
 n=int(input("enter number:"))
 fact=1
 for i in (1,n+1):
